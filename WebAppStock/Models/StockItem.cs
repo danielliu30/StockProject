83642d3a71d06906a6510ce6b7384365714b35c7
Daniel Liu <liu.5510@buckeyemail.osu.edu>
2019-08-02 15:12:43 -0400

    add stockItem to models

---
+++ WebAppStock/Models/StockItem.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebAppStock
+{
+    public class StockItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string TodaysDate { get; set; }
+
+        public float OpeningValue { get; set; }
+
+        public float HighValue { get; set; }
+
+        public float LowValue { get; set; }
+
+        public float CloseValue { get; set; }
+
+        public float AdjClose { get; set; }
+
+        public float Volume { get; set; }
+
+    }
+}
