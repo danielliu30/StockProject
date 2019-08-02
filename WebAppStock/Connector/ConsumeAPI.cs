7d47b18b6add8a7e4ccdecec3e04edf0a1b84905
Daniel Liu <liu.5510@buckeyemail.osu.edu>
2019-08-02 15:11:23 -0400

    Helper class to connect to api

---
+++ WebAppStock/Connector/ConsumeAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebAppStock.Connector
+{
+    public class ConsumeAPI
+    {
+        public HttpClient Initialize()
+        {
+            var Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:62504");
+            return Client;
+        }
+    }
+}
